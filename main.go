94b10ab6e9a2aca87d897a04061d84b3113c7130
Kazumasa Kohtaka <kkohtaka@gmail.com>
2018-07-06 09:30:16 +0900

    Implement secondFunction()

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	firstFunction()
+	secondFunction()
 }
 
 func firstFunction() {
 	fmt.Println("This is the first function.")
+}
+
+func secondFunction() {
+	fmt.Println("This is the second function.")
 }
